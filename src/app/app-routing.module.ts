eb26d03232838810f10d98ef69d1a7f0620d3f78
carlos731 <chtsca2001@gmail.com>
2022-09-01 10:08:29 -0300

    rota para component nav criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: NavComponent //Quando path da url da pagina (localhost:4200/) for vazio renderiza apenas o NavComponent a baixo.
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
