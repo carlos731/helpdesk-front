656d080eae8e1e2949a8358c2e2831f6b7c26b4e
carlos731 <chtsca2001@gmail.com>
2022-09-01 11:11:57 -0300

    Rota para component home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
-    path: '', component: NavComponent //Quando path da url da pagina (localhost:4200/) for vazio renderiza apenas o NavComponent a baixo.
+    path: '', component: NavComponent, children: [//Rota filha
+      {path: 'home', component: HomeComponent}
+    ]
   }
 ];
 
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }//Router permite que navegue entre os components
 
   ngOnInit(): void {
+    this.router.navigate(['home'])//Adicionar /home 
   }
 
 }
