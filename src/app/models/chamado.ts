1e94143346b7626f77dc1479c2ab0f065ebba1d4
carlos731 <chtsca2001@gmail.com>
2022-09-09 08:43:17 -0300

    Interface chamado criada

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,13 @@
+export interface chamado{
+    id?: any;
+    dataAbertura?: string;
+    dataFechamento?: string;
+    prioridade: string;
+    status: string;
+    titulo: string;
+    descricao: string;
+    tecnico: any;
+    cliente: any;
+    nomeCliente: string;
+    nomeTecnico: string;
+}
